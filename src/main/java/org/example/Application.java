f1a40082b1b7b150f07da502155bc406bea9c53c
Peemajoke <zinger.peem@gmail.com>
2023-08-14 19:39:53 +0700

    setter injection

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,14 +2,17 @@ package org.example;
 
 import org.example.services.SpeakerService;
 import org.example.services.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
+        //* Create applicationContext to boostrap our app with config from applicationContext.xml
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        //! Pain point 1: Hard Code Referencing SpeakerServiceImpl.
-        //! To change to new other class, even if it implements the same interface, rebuild is required.
-        //* Spring can make a factory to load SpeakerServiceImpl for us.
-        SpeakerService service = new SpeakerServiceImpl();
+        //        SpeakerService service = new SpeakerServiceImpl();
+        //* Get bean from appContext.
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstname());
         System.out.println(service.findAll().get(0).getLastname());
--- src/main/java/org/example/services/SpeakerServiceImpl.java
+++ src/main/java/org/example/services/SpeakerServiceImpl.java
@@ -1,19 +1,20 @@
 package org.example.services;
 
 import org.example.model.Speaker;
-import org.example.repository.HibernateSpeakerRepositoryImpl;
 import org.example.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    //! Pain point 2: Hard Code Referencing HibernateSpeakerRepositoryImpl.
-    //! Right now, HibernateSpeakerRepositoryImpl is a stub as DB is not yet establish. If DB is ready, we need to change line 14 to new the real repository that connects to the DB and RE-BUILD.
-    //* Spring can just let us change configuration w/o re-build anything. By changing configuration, spring change reference to the new class.
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    private SpeakerRepository repository;
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
