6549fe20b00f6854bac090c907ab0b8bf1a2e7de
Peemajoke <zinger.peem@gmail.com>
2023-08-14 19:53:45 +0700

    constructor injection

--- src/main/java/org/example/services/SpeakerServiceImpl.java
+++ src/main/java/org/example/services/SpeakerServiceImpl.java
@@ -6,9 +6,16 @@ import org.example.repository.SpeakerRepository;
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-
-
     private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(){
+
+    }
+
+    public  SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
